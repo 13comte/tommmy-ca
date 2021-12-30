28af0c8c60ecac244f5bf3232b1b1d3afd53e24e
Tommy Lecomte <lecomtetommy@gmail.com>
2021-12-30 10:23:48 -0500

    Updated file structure + anim. experiments

---
+++ source/scripts/index.js
@@ -0,0 +1,42 @@
+/*
+const canvas = document.getElementById("blurCanvas");
+const ctx = canvas.getContext("2d");
+let coord = { x: 0, y: 0 };
+let cycleCounter = 0;
+
+document.addEventListener("mousemove", start);
+document.addEventListener("mouseup", stop);
+window.addEventListener("resize", resize);
+
+resize();
+
+function resize() {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+}
+function reposition(event) {
+  coord.x = event.clientX - canvas.offsetLeft;
+  coord.y = event.clientY - canvas.offsetTop;
+}
+function start(event) {
+  document.addEventListener("mousemove", draw);
+  reposition(event);
+}
+function stop() {
+  document.removeEventListener("mousemove", draw);
+}
+function draw(event) {
+    cycleCounter++;
+  ctx.beginPath();
+  ctx.arc(coord.x, coord.y, 200, 0, Math.PI * 2);
+ctx.fillStyle = "rgba(0,0,0,0.01)";
+ctx.fill();
+  ctx.moveTo(coord.x, coord.y);
+  reposition(event);
+  ctx.lineTo(coord.x, coord.y);
+  if (cycleCounter == 200) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+        cycleCounter = 0;
+  }
+}
+*/
---
+++ source/scripts/styling.js
@@ -0,0 +1,7 @@
+// Cursor styling using the kursor library 
+// src -- https://lusaxweb.github.io/Kursor/
+new kursor({
+    type: 2,
+    removeDefaultCursor: true,
+    color: "#3f3f3f"
+});
